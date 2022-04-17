4796c7cefe63ec206eae2857c8816eaeb327f7c2
salih bozkaya <sbozkaya@natica.com.tr>
2022-04-17 21:15:34 +0300

    feat(middleware): middleware structure changed

--- src/middlewares/isSetCookie.ts
+++ src/middlewares/isSetCookie.ts
@@ -1,25 +1,32 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import jwt from "jsonwebtoken";
-import { user } from "../services";
 
 export function isSetCookie(req: Request, res: Response, next: NextFunction) {
     const cookie = req.cookies;
     if (!cookie?.qid) return res.sendStatus(httpStatus.UNAUTHORIZED);
     const refreshToken = cookie.qid;
 
-    return jwt.verify(
-        refreshToken,
-        process.env.REFRESH_TOKEN_SECRET_KEY as string,
-        async (err: any, decode: any) => {
-            console.log("err", err);
-            if (err) return res.sendStatus(httpStatus.FORBIDDEN);
-            const findedUser = await user.findOne({ email: decode.email });
+    try {
+        const payload = jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET_KEY as string
+        );
 
-            if (!findedUser) return res.sendStatus(httpStatus.FORBIDDEN);
-            req.refreshToken = refreshToken;
-
-            return next();
+        if (typeof payload === "string") {
+            console.log("payload type is string", payload);
+            return;
         }
-    );
+
+        req.payload = {
+            userId: payload.userId,
+            tokenVersion: payload.tokenVersion,
+        };
+        return next();
+    } catch (err) {
+        console.log("err", err);
+        return res.status(httpStatus.BAD_REQUEST).json({
+            message: "an error accured",
+        });
+    }
 }
