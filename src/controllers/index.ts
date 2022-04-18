5b8ef64359b6bfc13adb72c0b9bb4ee05e8c78d3
salih bozkaya <sbozkaya@natica.com.tr>
2022-04-18 11:27:04 +0300

    feat(nodejs.d.ts): .env file variables infered by typescript now

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,6 @@
 export { default as loginController } from "./loginController";
-export { default as meController } from "./meController";
-export { default as resetPasswordController } from "./resetPasswordController";
 export { default as logoutController } from "./logoutController";
-export { default as refreshController } from "./refreshController";
+export { default as meController } from "./meController";
+export { default as refreshTokenController } from "./refreshTokenController";
+export { default as resetPasswordController } from "./resetPasswordController";
 export { default as testControler } from "./test/testController";
--- src/types.d.ts
+++ src/types.d.ts
@@ -10,5 +10,29 @@ declare global {
                 [x: string]: any;
             };
         }
+
+        namespace NodeJS {
+            interface ProcessEnv {
+                DB_PORT: number;
+                PORT: number;
+                DB_NAME: string;
+                HOST: string;
+                DB_USER: string;
+                DB_PASSWORD: string;
+                NODE_ENV: string;
+                REDIS_URL: string;
+                ACCESS_TOKEN_SECRET_KEY: string;
+                REFRESH_TOKEN_SECRET_KEY: string;
+                SALT: number;
+                EMAIL_HOST: string;
+                EMAIL_PORT: number;
+                EMAIL_USER: string;
+                EMAIL_PASWORD: string;
+                EMAIL_FROM: string;
+                TOKEN_EXPIRE: number;
+                ORIGIN: string;
+            }
+        }
     }
 }
+export {};
