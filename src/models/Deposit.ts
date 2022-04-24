cbd81db72232c0003c37049446c19a824d89962c
salih_bozkaya <sbozkaya@natica.com.tr>
2022-04-25 00:11:44 +0300

    feat(deposit): deposit table implemented"

---
+++ src/models/Deposit.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { Currency, DepositStatusType } from "../types/types";
+import SuperEntity from "./SuperEntity";
+import VendorToDealerSiteToBuyerSite from "./VendorToDealerSiteToBuyerSite";
+
+@Entity("deposits")
+export default class Deposit extends SuperEntity {
+    @Column({ type: "real" })
+    public amount: number;
+
+    @Column({ length: 3, type: "varchar" })
+    public currency: Currency;
+
+    @Column({ type: "enum", enum: DepositStatusType })
+    public status: DepositStatusType;
+
+    @Column({ type: "timestamp", default: null })
+    public approval_date: Date;
+
+    /** relations */
+
+    @ManyToOne(() => VendorToDealerSiteToBuyerSite, (vdsbs) => vdsbs.deposits)
+    @JoinColumn({ name: "vdsbs_id" })
+    public vdsbs: VendorToDealerSiteToBuyerSite;
+}
