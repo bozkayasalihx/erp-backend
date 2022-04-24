bd7fbd3fd550b0a53274d8977a30524f65f0f836
salih_bozkaya <sbozkaya@natica.com.tr>
2022-04-25 00:07:21 +0300

    feat(advance) advance table added

---
+++ src/models/Advance.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { AdvanceStatusType, AdvanceType, Currency } from "../types/types";
+import SuperEntity from "./SuperEntity";
+import VendorToDealerSiteToBuyerSite from "./VendorToDealerSiteToBuyerSite";
+
+@Entity("advances")
+export default class Advance extends SuperEntity {
+    @Column({ type: "enum", enum: AdvanceType })
+    public advance_type: AdvanceType;
+
+    @Column({ type: "real" })
+    public amount: number;
+
+    @Column({ type: "varchar", length: 3 })
+    public currency: Currency;
+
+    @Column({ type: "enum", enum: AdvanceStatusType })
+    public status: AdvanceStatusType;
+
+    @Column({ type: "timestamp" })
+    public approval_date: Date;
+
+    /** relations */
+
+    @ManyToOne(() => VendorToDealerSiteToBuyerSite, (vdsbs) => vdsbs.advances)
+    @JoinColumn({ name: "vdsbs_id" })
+    public vdsbs: VendorToDealerSiteToBuyerSite;
+}
