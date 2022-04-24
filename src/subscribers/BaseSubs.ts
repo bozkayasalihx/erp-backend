e892fe7cd528ab05a0e404acdf6f4f4ca62d6bee
salih_bozkaya <sbozkaya@natica.com.tr>
2022-04-25 00:21:33 +0300

    fix(base_subs) base subscription listener renewed

--- src/subscribers/BaseSubs.ts
+++ src/subscribers/BaseSubs.ts
@@ -2,16 +2,13 @@ import {
     EntitySubscriberInterface,
     EventSubscriber,
     InsertEvent,
+    QueryRunner,
     Table,
+    TableForeignKey,
 } from "typeorm";
 import { appDataSource } from "../loaders";
 import UserEntityRelation from "../models/UserEntityRelation";
 import { UserTypes } from "../types/types";
-
-interface ITableOptions {
-    tableName: string;
-    refTableName: string;
-}
 
 @EventSubscriber()
 export class UserEntityRelationSubscriber
@@ -29,145 +26,10 @@ export class UserEntityRelationSubscriber
         return UserEntityRelation;
     }
 
-    async beforeInsert(event: InsertEvent<UserEntityRelation>) {
-        this.entity = event.entity;
-
-        const childType = await this.getChildType();
-
-        const valid = await event.queryRunner.hasColumn(
-            "user_entity_relation",
-            "ref_table_id"
-        );
-
-        if (valid) {
-            await event.queryRunner.dropTable(
-                "user_entity_relation",
-                true,
-                true,
-                true
-            );
-
-            if (childType === 1) {
-                // vendor
-                await event.queryRunner.createTable(
-                    this.genTable({
-                        tableName: "user_entity_relation",
-                        refTableName: "vendor",
-                    })
-                );
-            } else if (childType === 2) {
-                // dealer
-                await event.queryRunner.createTable(
-                    this.genTable({
-                        tableName: "user_entity_relation",
-                        refTableName: "dealer_site",
-                    })
-                );
-            } else if (childType === 3) {
-                // buyer
-                await event.queryRunner.createTable(
-                    this.genTable({
-                        tableName: "user_entity_relation",
-                        refTableName: "buyer_site",
-                    })
-                );
-            }
-        }
-    }
-
-    private genTable({ tableName, refTableName }: ITableOptions) {
-        return new Table({
-            name: tableName,
-            columns: [
-                {
-                    name: "id",
-                    type: "int",
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: "increment",
-                },
-                {
-                    name: "updated_at",
-                    type: "timestamp",
-                    default: "NOW()",
-                    onUpdate: "NOW()",
-                },
-
-                {
-                    name: "created_at",
-                    type: "timestamp",
-                    default: "NOW()",
-                },
-                {
-                    name: "start_date",
-                    isNullable: true,
-                    default: null,
-                    type: "timestamp",
-                },
-                {
-                    name: "end_date",
-                    isNullable: true,
-                    default: null,
-                    type: "timestamp",
-                },
-                {
-                    name: "description",
-                    type: "varchar",
-                    length: "240",
-                    default: null,
-                },
-                {
-                    name: "created_by",
-                    type: "int",
-                },
-
-                {
-                    name: "updated_by",
-                    type: "int",
-                },
-
-                {
-                    name: "ref_table_id",
-                    type: "int",
-                },
-                {
-                    name: "user_id",
-                    type: "int",
-                },
-            ],
-            foreignKeys: [
-                {
-                    name: "ref_table",
-                    referencedTableName: refTableName,
-                    referencedColumnNames: ["id"],
-                    columnNames: ["ref_table_id"],
-                },
-                {
-                    name: "created_by",
-                    referencedTableName: "user_tbl",
-                    referencedColumnNames: ["id"],
-                    columnNames: ["created_by"],
-                },
-                {
-                    name: "updated_by",
-                    referencedTableName: "user_tbl",
-                    referencedColumnNames: ["id"],
-                    columnNames: ["updated_by"],
-                },
-                {
-                    name: "users",
-                    referencedTableName: "user_tbl",
-                    referencedColumnNames: ["id"],
-                    columnNames: ["user_id"],
-                },
-            ],
-        });
-    }
-
     private async getChildType() {
         const user: [{ id: number; user_type: UserTypes }] =
             await this.manager.query(
-                `SELECT id, user_type FROM user_tbl where id = ${this.entity.users.id}`
+                `SELECT id, user_type FROM user_tbl where id = ${this.entity.user.id}`
             );
         if (!user) throw new Error("no such as user");
         return user[0].user_type === UserTypes.VENDOR ||
@@ -182,10 +44,61 @@ export class UserEntityRelationSubscriber
             : 0;
     }
 
-    // afterLoad(
-    //     entity: UserEntityRelation,
-    //     event: LoadEvent<UserEntityRelation>
-    // ) {
-    //     console.log("entity", entity);
-    // }
+    private async dropRelation(queryRunner: QueryRunner) {
+        const table = await this.getTable(queryRunner);
+        const ref_table_foreignkey = table.foreignKeys.filter(
+            (key) => key.columnNames[0] === "ref_table_id"
+        )[0];
+
+        return await queryRunner.dropForeignKey(table, ref_table_foreignkey);
+    }
+
+    private async getTable(queryRunner: QueryRunner) {
+        const table = (await queryRunner.getTable(
+            "user_entity_relation"
+        )) as Table;
+
+        return table;
+    }
+
+    private async createRelation(
+        queryRunner: QueryRunner,
+        referedTable: string
+    ) {
+        const foreignKey = new TableForeignKey({
+            columnNames: ["ref_table_id"],
+            referencedColumnNames: ["id"],
+            referencedTableName: referedTable,
+            onDelete: "CASCADE",
+        });
+        const table = await this.getTable(queryRunner);
+
+        return await queryRunner.createForeignKey(table, foreignKey);
+    }
+
+    async beforeInsert(event: InsertEvent<UserEntityRelation>) {
+        const { entity, metadata, queryRunner } = event;
+        // entity our table ;
+        this.entity = entity;
+
+        metadata.relations.filter((relation) => {
+            relation.isManyToOne || relation.isOneToMany;
+        });
+
+        const childtype = await this.getChildType();
+
+        if (childtype === 1) {
+            // vendor
+            await this.dropRelation(queryRunner);
+            await this.createRelation(queryRunner, "vendor");
+        } else if (childtype === 2) {
+            // deadler
+            await this.dropRelation(queryRunner);
+            await this.createRelation(queryRunner, "dealer_site");
+        } else if (childtype === 3) {
+            // buyer site;
+            await this.dropRelation(queryRunner);
+            await this.createRelation(queryRunner, "buyer_site");
+        }
+    }
 }
