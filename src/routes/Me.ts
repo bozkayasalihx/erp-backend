b32e8403ee57b9e2b81015b3003e615326be5c4f
salih_bozkaya <sbozkaya@natica.com.tr>
2022-04-25 17:54:04 +0300

    fix(user_related): user related folder structure renewed

--- src/routes/Me.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { meController } from "../controllers";
-import { authenticate } from "../middlewares/authenticate";
-
-const meRouter = Router();
-meRouter.get("/me", authenticate, meController);
-export default meRouter;
