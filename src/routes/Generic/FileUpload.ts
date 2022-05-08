205a658cbac32c8199cbfbc2c65748be64a54514
salih_bozkaya <sbozkaya@natica.com.tr>
2022-05-08 22:31:38 +0300

    feat(file_upload): file upload middleware fixed and new routes enum's added

--- src/routes/Generic/FileUpload.ts
+++ src/routes/Generic/FileUpload.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
+import fileUpload from "express-fileupload";
 import { fileUploadController } from "../../controllers";
 import checkFileType from "../../middlewares/checkFileType";
+import { __prod__ } from "../../scripts/dev";
 import { Routes } from "../../types/routePath";
 
 const router = Router();
 // xlsx
 // csv
-router.post(Routes.FILE_UPLOAD, checkFileType, fileUploadController);
+const file = () => {
+    return fileUpload({
+        limits: {
+            fileSize: 10 * 1024 * 1024, // 10mb
+        },
+        useTempFiles: true,
+        tempFileDir: "/temp/",
+        debug: !__prod__,
+    });
+};
+router.post(Routes.FILE_UPLOAD, file(), checkFileType, fileUploadController);
 
 export default router;
