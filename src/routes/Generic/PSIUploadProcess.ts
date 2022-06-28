efab2c0f061037b6c4fbec8ced3e888e84fdeeb7
salih bozkaya <sbozkaya@natica.com.tr>
2022-06-28 11:37:30 +0300

    feat(psi_vi): insertion machanism of payment schedule and invoice changed to bulk to rollback machanism

--- src/routes/Generic/PSIUploadProcess.ts
+++ src/routes/Generic/PSIUploadProcess.ts
@@ -4,16 +4,15 @@ import { Router } from "express";
 import fileUpload, { UploadedFile } from "express-fileupload";
 import { createReadStream } from "fs";
 import httpStatus from "http-status";
-import { InsertResult } from "typeorm";
 import { checkFileType } from "../../middlewares";
-import { PaymentScheduleInterface } from "../../models";
+import { PaymentSchedule, PaymentScheduleInterface } from "../../models";
 import { __prod__ } from "../../scripts/dev";
-import parseError from "../../scripts/errors/parseError";
 import CsvParser from "../../scripts/parser/csvParser";
 import DataVerifier from "../../scripts/parser/dataVerifier";
 import { GenerateCsv } from "../../scripts/parser/generateCsv";
+import DatabaseTransaction from "../../scripts/transection/databaseTransection";
 import { increment } from "../../scripts/utils/revokeRefreshToken";
-import { invoiceOperation, paymentOperation } from "../../services";
+import { invoiceOperation } from "../../services";
 import { TypedResponse } from "../../types";
 import { Routes } from "../../types/routePath";
 import { OmmitedPaymentSchedule } from "./type";
@@ -61,6 +60,8 @@ router.post(
         const parser = new CsvParser<PaymentScheduleInterface>(DELIMITER);
 
         parser.readData(data);
+        // @ts-ignore
+        // eslint-disable-next-line consistent-return
         parser.matcher(async (err, parsedData) => {
             if (err || !parsedData) {
                 return res.status(httpStatus.BAD_REQUEST).json({
@@ -68,109 +69,122 @@ router.post(
                 });
             }
 
-            const insertedPSIResp: Array<Args<OmmitedPaymentSchedule>> = [];
-            const insertQueue: Array<Promise<PaymentScheduleInterface>> = [];
-            for (const record of parsedData) {
-                insertQueue.push(
-                    paymentOperation.PSIRepo.save(
-                        {
-                            ...record,
-                            file_name: fileName,
-                            file_process_id: fileProcessid,
-                            created_by: user,
-                            updated_by: user,
-                        },
-                        { chunk: 5 }
-                    )
-                );
-            }
-
             try {
-                const temp = await Promise.all(insertQueue);
+                DatabaseTransaction.connect();
+                DatabaseTransaction.transection(async (error, queryRunner) => {
+                    if (error) {
+                        return res.status(httpStatus.BAD_REQUEST).json({
+                            message: "bad request",
+                        });
+                    }
 
-                for (let i = 0; i < temp.length; i++) {
-                    const {
-                        created_by,
-                        updated_by,
-                        created_at,
-                        updated_at,
-                        ...args
-                    } = temp[i];
-                    insertedPSIResp.push(args);
-                }
+                    const insertedPSIResp: Array<Args<OmmitedPaymentSchedule>> =
+                        [];
+                    const insertQueue: Array<PaymentScheduleInterface> = [];
+
+                    for (const record of parsedData) {
+                        insertQueue.push(
+                            PaymentScheduleInterface.create({
+                                ...record,
+                                file_name: fileName,
+                                file_process_id: fileProcessid,
+                                created_by: user,
+                                updated_by: user,
+                            })
+                        );
+                    }
+
+                    const temp = (await queryRunner?.manager.save(insertQueue, {
+                        chunk: 5,
+                    })) as Array<PaymentScheduleInterface>;
+
+                    for (let i = 0; i < temp.length; i++) {
+                        const {
+                            created_by,
+                            updated_by,
+                            created_at,
+                            updated_at,
+                            ...args
+                        } = temp[i];
+                        insertedPSIResp.push(args);
+                    }
+
+                    const psiDataVerifier = new DataVerifier("psi");
+
+                    psiDataVerifier.setPSIData(insertedPSIResp);
+                    psiDataVerifier.validate();
+
+                    if (psiDataVerifier.errors.size) {
+                        const generateCsv =
+                            new GenerateCsv<PaymentScheduleInterface>(
+                                psiDataVerifier.errors,
+                                parsedData
+                            );
+                        await generateCsv.genereate(fileName);
+
+                        return res.status(httpStatus.BAD_REQUEST).json({
+                            message:
+                                "invalid column type detected please fix that type and try again",
+                        });
+                    }
+
+                    const psi = psiDataVerifier.getPSIData[0];
+
+                    const vdsbsId = parseInt(psi.vdsbs_id as string, 10);
+                    const invoiceNo = psi.invoice_no as string;
+
+                    const invoice = await invoiceOperation.invoiceRepo.findOne({
+                        where: { vdsbsId, invoiceNo },
+                    });
+                    if (!invoice) {
+                        return res.status(httpStatus.BAD_REQUEST).json({
+                            message: "bad request",
+                        });
+                    }
+
+                    const queue: Array<PaymentSchedule> = [];
+                    for (
+                        let i = 0;
+                        i < psiDataVerifier.getPSIData.length;
+                        i++
+                    ) {
+                        const currentPSI = psiDataVerifier.getPSIData[i];
+                        queue.push(
+                            PaymentSchedule.create({
+                                currency: currentPSI.currency as string,
+                                invoice,
+                                vdsbsId,
+                                dueDate: new Date(
+                                    currentPSI.due_date as string
+                                ),
+                                dueAmount: parseFloat(
+                                    currentPSI.due_amount as string
+                                ),
+                                lineNo: parseInt(
+                                    currentPSI.line_no as string,
+                                    10
+                                ),
+                                created_by: user,
+                                updated_by: user,
+                            })
+                        );
+                    }
+
+                    await queryRunner?.manager.save(queue);
+
+                    return res.status(httpStatus.OK).json({
+                        message: "operation succesful",
+                    });
+                });
             } catch (err1) {
-                return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-                    message: "an error accured try again later",
-                });
-            }
-
-            const psiDataVerifier = new DataVerifier("psi");
-
-            psiDataVerifier.setPSIData(insertedPSIResp);
-            psiDataVerifier.validate();
-
-            if (psiDataVerifier.errors.size) {
-                const generateCsv = new GenerateCsv<PaymentScheduleInterface>(
-                    psiDataVerifier.errors,
-                    parsedData
-                );
-                await generateCsv.genereate(fileName);
-
-                return res.status(httpStatus.BAD_REQUEST).json({
-                    message:
-                        "invalid column type detected please fix that type and try again",
-                });
-            }
-
-            const psi = psiDataVerifier.getPSIData[0];
-
-            const vdsbsId = parseInt(psi.vdsbs_id as string, 10);
-            const invoiceNo = psi.invoice_no as string;
-
-            const invoice = await invoiceOperation.invoiceRepo.findOne({
-                where: { vdsbsId, invoiceNo },
-            });
-            if (!invoice) {
-                return res.status(httpStatus.BAD_REQUEST).json({
-                    message: "bad request",
-                });
-            }
-
-            const queue: Array<Promise<InsertResult>> = [];
-            for (let i = 0; i < psiDataVerifier.getPSIData.length; i++) {
-                const currentPSI = psiDataVerifier.getPSIData[i];
-                queue.push(
-                    paymentOperation.psRepo.insert({
-                        currency: currentPSI.currency as string,
-                        invoice,
-                        vdsbsId,
-                        dueDate: new Date(currentPSI.due_date as string),
-                        dueAmount: parseFloat(currentPSI.due_amount as string),
-                        lineNo: parseInt(currentPSI.line_no as string, 10),
-                        created_by: user,
-                        updated_by: user,
-                    })
-                );
-            }
-
-            try {
-                await Promise.all(queue);
-
-                return res.status(httpStatus.OK).json({
-                    message: "operation succesful",
-                });
-            } catch (erro1) {
-                parseError.setError(erro1?.detail);
-                const d = parseError.parser();
-                if (!d) {
-                    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-                        message: "an error accured try again later",
+                if (err1?.detail?.includes("already exists")) {
+                    // already exits error;
+                    return res.status(httpStatus.BAD_REQUEST).json({
+                        message: "this record already exists",
                     });
                 }
-
-                return res.status(httpStatus.BAD_REQUEST).json({
-                    message: "some of yours inputs is invalid",
-                    data: { message: erro1?.detail },
+                return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                    message: "an error accured try again later",
                 });
             }
         });
