bea43eecc61f8fd027dea580853e2e9e0a2e5241
salih bozkaya <sbozkaya@natica.com.tr>
2022-07-04 17:15:20 +0300

    fix(git): git installed

--- containers/deploy.sh
+++ containers/deploy.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-SHA=$(git rev-parse HEAD)
-
 docker build -f ./containers/Dockerfile -t naticadevelopment/prod_server:latest -t naticadevelopment/prod_server:$SHA . 
 docker build -f ./containers/Dockerfile.prod.postgres -t naticadevelopment/prod_db:latest -t naticadevelopment/prod_db:$SHA . 
 
