fafeccdf54fe3cd852a350c6ab0f952ec8e1cc1a
salih bozkaya <sbozkaya@natica.com.tr>
2022-07-26 13:11:33 +0300

    fix(views): removed old tableview related tabled and controller

--- src/routes/UserAccess/UserAccessRoute.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getUserEntityAccessController } from "../../controllers";
-import { Routes } from "../../types/routePath";
-
-const router = Router();
-
-router.get(Routes.GET_USER_ACCESS, getUserEntityAccessController);
-
-export default router;
